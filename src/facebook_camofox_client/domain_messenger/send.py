38c9e33e6ff364ab815f3d6a1e8e2c170b660fcf
matthewdonsemail-lab <matthewdonsemail@gmail.com>
2026-08-18 13:25:33 +0700

    Open Messenger thread before sending

--- src/facebook_camofox_client/domain_messenger/send.py
+++ src/facebook_camofox_client/domain_messenger/send.py
@@ -7,9 +7,12 @@ from facebook_camofox_client.domain_camofox.interactions import CamofoxInteracti
 from facebook_camofox_client.domain_camofox.selectors import MESSAGE_COMPOSER
 from .compose import compose_message
 from .schemas import SendMessageInput, SendMessageOutput
+from .threads import open_thread
 
 
 async def send_message(page: Any, data: SendMessageInput) -> SendMessageOutput:
+    if data.thread_name and not await open_thread(page, data.thread_name):
+        return SendMessageOutput(sent=False, thread_id=data.thread_id, error="thread_not_found")
     if not await compose_message(page, data.text):
         return SendMessageOutput(sent=False, thread_id=data.thread_id, error="composer_not_found")
     sent = await CamofoxInteractions(page).press(MESSAGE_COMPOSER, "Enter")
