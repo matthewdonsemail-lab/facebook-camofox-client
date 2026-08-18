7eb6f3bfa50e21302e913a48cf3a742bd1091abb
matthewdonsemail-lab <matthewdonsemail@gmail.com>
2026-08-18 13:24:36 +0700

    Export Facebook account domain primitives

--- src/facebook_camofox_client/domain_accounts/__init__.py
+++ src/facebook_camofox_client/domain_accounts/__init__.py
@@ -1,1 +1,16 @@
-"""Domain: Account management and auth."""
+"""Facebook account authentication domain."""
+
+from .auth_guard import AuthGuard, AuthGuardResult, AuthState
+from .challenges import AuthenticationChallenges, ChallengeResult
+from .login import FacebookLogin
+from .models import Account
+
+__all__ = [
+    "Account",
+    "AuthGuard",
+    "AuthGuardResult",
+    "AuthState",
+    "AuthenticationChallenges",
+    "ChallengeResult",
+    "FacebookLogin",
+]
